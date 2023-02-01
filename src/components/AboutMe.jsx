2dc0fc0fd372eef64651b7d74609d193524da626
fabriciomarote <fabrii.cai93@gmail.com>
2023-01-31 23:51:48 -0300

    Se modifica estilo y se cambia cv en Contact

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -26,8 +26,8 @@ const AboutMe = ( props ) => {
                     </div> 
                     <div className='text-box span1 wow animate__animated animate__zoomIn animate__delay-0.5s'>    
                         <p>¡Hola! Me llamo Fabricio y soy de Buenos Aires, Argentina. Soy Desarrollador Web Full Stack y estoy en busca de mi primera experiencia laboral en IT. Actualmente soy estudiante avanzado de la Tecnicatura Universitaria en Programación Informática de la Universidad Nacional de Quilmes, y durante este período aprendí tecnologías tanto de Frontend como de Backend. </p> 
-                        <p>He desarrollado varios proyectos utilizando diferentes lenguajes y tecnologías, tengo experiencia y un buen manejo en lo que respecta al trabajo en equipo, tambien tengo conocimientos sobre las metodologías ágiles (SCRUM).</p>
                         <p>Me considero una persona responsable, creativa, detallista y con muchas ganas de seguir aprendiendo nuevas habilidades y tecnologías para mi desarrollo profesional. También tengo un fuerte compromiso con los proyectos que me asignan, con el trabajo en equipo, y doy lo mejor de mí para completar las tareas con el mejor resultado posible.</p>
+                        <p>He desarrollado varios proyectos utilizando diferentes lenguajes y tecnologías, tengo experiencia y un buen manejo en lo que respecta al trabajo en equipo, tambien tengo conocimientos sobre las metodologías ágiles (SCRUM).</p>
                     </div>
                 </div>
             </div>
