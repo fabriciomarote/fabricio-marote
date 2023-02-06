1a61c1fae19b217e3f3ecace787a80e6f0240d41
fabriciomarote <fabrii.cai93@gmail.com>
2023-02-06 19:16:57 -0300

    Se modifica estilo en navbar y se implementa para pantalla media y grande

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -16,35 +16,16 @@ const Navbar2 = ( props ) => {
             <div className="navbar-container " data-theme={theme}>
                 <div className="nav">
                     <input type="checkbox" id="nav-check"/>
-                    <div className="nav-header">
+                    <div className="nav-header2">
                         <div className="box-logo">  
                             <a href="/" className='logo'><img src={logo} alt="logo" /></a>
                         </div>
-                    </div>
-                    <div className="nav-btn2">
                         <div className="theme-3">
                             <div className='icon-theme' onClick={() => changeMode()}>
                                 { theme === "dark" ? <BsFillMoonFill className='icon-dark' size={25}/> : <BsFillSunFill className='icon-light' size={25}/>}
                             </div>
                         </div>
-                        <label className="nav-check" htmlFor="nav-check">
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </label>
-                    </div>
-                    <div className="nav-links">                  
-                        <a href="/#home" className="btn-btn hvr-underline-from-left" >Inicio</a>
-                        <a href="/#aboutMe" className="btn-btn hvr-underline-from-left" >Sobre mí</a>
-                        <a href="/#skills" className="btn-btn hvr-underline-from-left" >Habilidades</a>
-                        <a href="/#projects" className="btn-btn hvr-underline-from-left">Proyectos</a>
-                        <a href="/#contact" className="btn-btn hvr-underline-from-left"> Contacto</a> 
-                        <div className="theme-2">
-                            <div className='icon-theme' onClick={() => changeMode()}>
-                                { theme === "dark" ? <BsFillMoonFill  className='icon-dark' size={35}/> : <BsFillSunFill className='icon-light' size={35}/>}
-                            </div>
-                        </div>
-                    </div>    
+                    </div> 
                 </div>
             </div>  
     );
